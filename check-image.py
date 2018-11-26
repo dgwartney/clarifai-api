a2e18e5bbdf3d537c3fe73dd96d311d52f4607f1
david gwartney <david.gwartney@avaamo.com>
2018-11-26 11:28:57 -0800

    Get URL from command line

--- check-image.py
+++ check-image.py
@@ -18,5 +18,5 @@ model = app.models.get("general-v1.3")
       
 # predict with the model
 # result = model.predict_by_url(url=url)
-result = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+result = model.predict_by_url(url=url)
 print(json.dumps(result))
